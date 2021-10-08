52e87e4a508abb98db5887b9d57b355c8abb1be7
RukhsarAhmed <rukhsar101095@gmail.com>
2021-10-08 23:30:35 +0530

    setup updated

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-PROJECT_NAME="Onenuron_pypi"
+PROJECT_NAME="perceptron_pypi"
 USER_NAME= "Rukhsar111"
 
 
